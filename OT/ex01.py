3ff6c8460d365daf57fd1a0609d54f868d14b711
eogml0217 <daehee159753@gmail.com>
2021-10-01 21:11:52 +0900

    과제

---
+++ OT/ex01.py
@@ -0,0 +1,28 @@
+# A=1
+# B=6
+# print(A+B)
+# print(A-B)
+# print(A*B)
+# print(A/B)
+# print(A%B)
+
+A=472
+B=385
+####1
+# C=str(B)
+#
+# print(A*int(C[2]))
+# print(A*int(C[1]))
+# print(A*int(C[0]))
+# print(A*B)
+
+###2
+print(B//100)
+print(B//10%10)
+print(B%10)
+
+print(A*(B%10))
+print(A*(B//10%10))
+print(A*(B//100))
+
+print(A*B)
